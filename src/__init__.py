2eff63698f1a70a092c0684a3f1a4a0080f9cb69
Henrik Giesel <hengiesel@gmail.com>
2020-09-09 11:27:10 +0200

    Make shortcuts settable

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from .webview import init_webview
 from .editor import init_editor
+from .addon_manager import init_addon_manager
 
 def init():
+    init_addon_manager()
     init_webview()
     init_editor()
